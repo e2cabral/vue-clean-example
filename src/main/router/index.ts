c545f597cf6c9b55729251f760c36711a36c4aaf
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-23 11:05:26 -0300

    🚧 remove about route

--- src/main/router/index.ts
+++ src/main/router/index.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../../presentation/views/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -8,15 +7,7 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../../presentation/views/About.vue'),
+    component: () => import('../../presentation/views/Home.vue'),
   },
 ];
 
