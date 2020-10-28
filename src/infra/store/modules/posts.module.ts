4b68b96b4867601e15838f4ce500a8b5b5ac1f08
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-28 00:13:04 -0300

    🚧 add methods to manipulate just one post

--- src/infra/store/modules/posts.module.ts
+++ src/infra/store/modules/posts.module.ts
@@ -1,11 +1,13 @@
-import GET_POSTS from '@/infra/store/types/actions/actions.type';
-import SET_POSTS from '@/infra/store/types/mutations/mutations.type';
+import { GET_POSTS, GET_POST } from '@/infra/store/types/actions/actions.type';
+import { SET_POSTS, SET_POST } from '@/infra/store/types/mutations/mutations.type';
 import { Post } from '@/domain/models/post-model';
 import { PostService } from '@/data/protocols/posts/post-service';
 
-export const state: { posts: Array<Post> } = { posts: [] };
+export const state: { posts: Array<Post>; post: Post | null } = { posts: [], post: null };
 
+// <editor-fold desc="Actions">
 export const actions = {
+  // @desc Return all posts stored
   async [GET_POSTS](context: { commit: Function }): Promise<Array<Post> | Error> {
     try {
       const posts = await PostService.getPosts();
@@ -16,19 +18,44 @@ export const actions = {
       return new Error(error);
     }
   },
-};
 
+  // @desc Return an specific post
+  async [GET_POST](context: { commit: Function }, id: number): Promise<Post | Error> {
+    try {
+      const post = await PostService.getPost(id);
+      context.commit(SET_POST, post);
+      return post;
+    } catch (error) {
+      console.log(error);
+      return new Error(error);
+    }
+  },
+};
+// </editor-fold>
+
+// <editor-fold desc="Mutations">
 export const mutations = {
   [SET_POSTS](state: { posts: Array<Post> }, posts: Array<Post>) {
     state.posts = posts;
   },
-};
 
+  [SET_POST](state: { post: Post }, post: Post) {
+    state.post = post;
+  },
+};
+// </editor-fold>
+
+// <editor-fold desc="Getters">
 const getters = {
   posts(state: { posts: Array<Post> }): Array<Post> {
     return state.posts;
   },
+
+  post(state: { post: Post }): Post {
+    return state.post;
+  },
 };
+// </editor-fold>
 
 export default {
   state,
