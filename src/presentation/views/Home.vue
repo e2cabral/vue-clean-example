563f9aa2e705f86c828ad8ba007d3f809b7c6a08
Edson Cabral <edson.cabral@trescon.com.br>
2020-10-28 00:14:09 -0300

    💄 change the UI to show the clicked post

--- src/presentation/views/Home.vue
+++ src/presentation/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="home">
-    <ul>
-      <li v-for="post in posts" :key="post.id">
+    <ul style="width: 80%; text-align: center;">
+      <li
+        v-for="post in posts"
+        :key="post.id"
+        @click="dispatchPost(post.id)"
+        style="padding: 2rem 0;">
         <strong>{{ post.title }}</strong><br>
         {{ post.body }}<br>
       </li>
     </ul>
+    <div v-if="post">
+      {{ post }}
+    </div>
   </div>
 </template>
 
@@ -13,20 +20,32 @@
 import 'reflect-metadata';
 import { Component, Vue } from 'vue-property-decorator';
 import { Post } from '@/domain/models/post-model';
-import GET_POSTS from '@/infra/store/types/actions/actions.type';
+import { GET_POSTS, GET_POST } from '@/infra/store/types/actions/actions.type';
 
 @Component
 export default class Home extends Vue {
+  // <editor-fold desc="Computed">
   get posts(): Array<Post> {
     return this.$store.getters.posts;
   }
 
+  get post(): Post {
+    return this.$store.getters.post;
+  }
+  // </editor-fold>
+
+  // <editor-fold desc="Mounted">
   mounted() {
-    this.getPosts();
+    this.dispatchPosts();
+  }
+  // </editor-fold>
+
+  dispatchPosts() {
+    this.$store.dispatch(GET_POSTS);
   }
 
-  getPosts() {
-    this.$store.dispatch(GET_POSTS);
+  dispatchPost(id: number) {
+    this.$store.dispatch(GET_POST, id);
   }
 }
 </script>
